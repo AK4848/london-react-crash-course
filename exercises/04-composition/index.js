e7997ee3db65e25f73b07ccca4cb9d0d6d06a5ed
Attila Keri <attila.keri@icloud.com>
2016-11-10 19:40:44 +0000

    4th exercise done

--- exercises/04-composition/index.js
+++ exercises/04-composition/index.js
@@ -3,14 +3,30 @@ import React from 'react';
 class List extends React.Component {
   // TODO write your component here
   render() {
-    return null;
+    let classNames = [
+      "list-group",
+      "list-group-item"
+    ]
+    return <div>
+      <ul className={classNames[0]}>
+        <li className={classNames[1]}>{this.props.topic}</li>
+      </ul>
+    </div>;
   }
 }
 
 class Topics extends React.Component {
   // TODO write your component here
   render() {
-    return null;
+    let topics = [
+  'Component',
+  'Props',
+  'Composition'
+]
+  let maps = topics.map(function(n){
+    return <List topic={n} />
+  })
+    return <div>{maps}</div>;
   }
 }
 
